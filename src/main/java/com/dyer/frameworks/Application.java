39e11841377719262fb12444face9e929b8f7731
cdyer <chris.dyer@lowes.com>
2017-01-05 13:07:40 -0500

    Renaming context file.

--- src/main/java/com/dyer/frameworks/Application.java
+++ src/main/java/com/dyer/frameworks/Application.java
@@ -10,10 +10,10 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @ComponentScan
 @EnableAutoConfiguration
-public class ApplicationConfig {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApplicationConfig.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
